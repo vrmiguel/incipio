22185cc78e1ae439f1988f5a8c9ba87888c21520
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2022-12-21 11:10:14 -0300

    Use `cstr!` where applicable

--- src/mount.rs
+++ src/mount.rs
@@ -1,38 +1,41 @@
 use nix::mount::{mount, MsFlags};
+use cstr::cstr;
 
 pub fn mount_pseudo_filesystems() -> nix::Result<()> {
+    let mode0755 = cstr!("mode=0755");
+
     // Mounting procfs
     mount(
-        Some("proc"),
-        "/proc",
-        Some("proc"),
+        Some(cstr!("proc")),
+        cstr!("/proc"),
+        Some(cstr!("proc")),
         MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV,
         None as Option<&str>,
     )?;
 
     // Mounting sys
     mount(
-        Some("sys"),
-        "/sys",
-        Some("sysfs"),
+        Some(cstr!("sys")),
+        cstr!("/sys"),
+        Some(cstr!("sysfs")),
         MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV,
         None as Option<&str>,
     )?;
 
     mount(
-        Some("run"),
-        "/run",
-        Some("tmpfs"),
+        Some(cstr!("run")),
+        cstr!("/run"),
+        Some(cstr!("tmpfs")),
         MsFlags::MS_NOSUID | MsFlags::MS_NODEV,
-        Some("mode=0755"),
+        Some(mode0755),
     )?;
 
     mount(
-        Some("dev"),
-        "/dev",
-        Some("devtmpfs"),
+        Some(cstr!("dev")),
+        cstr!("/dev"),
+        Some(cstr!("devtmpfs")),
         MsFlags::MS_NOSUID,
-        Some("mode=0755"),
+        Some(mode0755),
     )?;
 
     Ok(())
